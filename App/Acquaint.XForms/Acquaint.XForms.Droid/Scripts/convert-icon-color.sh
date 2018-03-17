e763421b7b32218d10f836443198b94a2a37edec
Steve Busi <steve.busi@dudesolutions.com>
2018-03-16 20:53:08 -0400

    post clone scripts

---
+++ App/Acquaint.XForms/Acquaint.XForms.Droid/Scripts/convert-icon-color.sh
@@ -0,0 +1,18 @@
+﻿#!/bin/bash
+
+# ./convert_icon_color.sh 
+# in the folder with the files you want to convert
+
+brew list ImageMagick || brew install ImageMagick
+
+ORANGE="rgb(255,106,0)"
+AEBLUE="rgb(16,87,123)"
+
+png="../Resources/drawable/icon.png"
+#for png in *.png
+#do
+    # To, From
+    convert $png -fuzz 20% -fill $ORANGE -opaque $AEBLUE "$png"_
+    echo $png
+    mv "$png"_ $png
+#done
